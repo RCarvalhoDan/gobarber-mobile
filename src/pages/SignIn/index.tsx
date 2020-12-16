d8ed8cc360d4429e528ef90d22d99adb0fa7b862
Daniel Carvalho <carvalho.rochadan@gmail.com>
2020-12-15 23:13:11 -0300

    Turn off react/prop-types eslint rule. Add components directory. Add Button component. Add input component. Add Button and Input to SignIn.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import logoImg from '../../assets/logo.png';
 
 import { Container, Title } from './styles';
@@ -10,6 +13,10 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu login</Title>
+      <Input />
+      <Input />
+
+      <Button>Entrar</Button>
     </Container>
   );
 };
